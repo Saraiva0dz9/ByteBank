e0b3c45c6aa89f330ca5ab32084cf0171ff35b1a
Leonardo Saraiva <leonardosaraiva1911@gmail.com>
2022-12-29 09:13:55 -0300

    Initial commit

---
+++ bytebank/Program.cs
@@ -0,0 +1,9 @@
+﻿using bytebank;
+
+ContaCorrente contaDoLeonardo = new ContaCorrente();
+contaDoLeonardo.titular = "Leonardo Saraiva";
+contaDoLeonardo.numeroAgencia = 09;
+contaDoLeonardo.conta = "1010-x";
+contaDoLeonardo.saldo = 100;
+
+Console.WriteLine("Saldo da conta do Leonardo = " +contaDoLeonardo.saldo);
