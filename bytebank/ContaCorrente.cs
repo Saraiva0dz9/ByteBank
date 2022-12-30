8d95bfa9f2780e69bf7a3eff4ac21882bffb4311
Leonardo Saraiva <leonardosaraiva1911@gmail.com>
2022-12-29 21:49:21 -0300

    Feat: Adicionado métodos de saque e transferência

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+ 
 namespace bytebank
 {
     public class ContaCorrente
@@ -13,9 +11,54 @@ namespace bytebank
         public string titular;
         public double saldo;
 
+        public static void Menu()
+        {
+            ContaCorrente contaDoLeonardo = new ContaCorrente
+            {
+                titular = "Leonardo Saraiva",
+                numeroAgencia = 09,
+                conta = "1010-x",
+                saldo = 100
+            };
+
+            Console.WriteLine($"Titular da conta: {contaDoLeonardo.titular}");
+            Console.WriteLine($"Número da conta: {contaDoLeonardo.conta}");
+            Console.WriteLine($"Agência: {contaDoLeonardo.numeroAgencia}");
+            Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", contaDoLeonardo.saldo)}");
+
+            Console.ReadKey();
+        }
+
         public void Depositar(double valor)
         {
             this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
         }
     }
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -6,8 +6,27 @@ contaDoLeonardo.numeroAgencia = 09;
 contaDoLeonardo.conta = "1010-x";
 contaDoLeonardo.saldo = 100;
 
-Console.WriteLine("Saldo da conta do Leonardo = " +contaDoLeonardo.saldo);
+Console.WriteLine("Saldo da conta do Leonardo = " + contaDoLeonardo.saldo);
 
-contaDoLeonardo.Depositar(100);
+ContaCorrente contaDaKaty = new ContaCorrente();
+contaDaKaty.titular = "Katy Victoria";
+contaDaKaty.numeroAgencia = 10;
+contaDaKaty.conta = "1010-x";
+contaDaKaty.saldo = 400;
 
-Console.WriteLine("Saldo da conta do Leonardo pós-deposito = " + contaDoLeonardo.saldo);
+
+Console.WriteLine("Saldo da conta da Katy = " + contaDaKaty.saldo);
+
+contaDoLeonardo.Transferir(50, contaDaKaty);
+Console.WriteLine("Saldo do Leonardo = "+ contaDoLeonardo.saldo);
+Console.WriteLine("Saldo do Katy = "+ contaDaKaty.saldo);
+
+
+
+//ContaCorrente conta = new ContaCorrente();
+//Console.WriteLine($"Titular da conta: {contaDoLeonardo.titular}");
+//Console.WriteLine($"Número da conta: {contaDoLeonardo.conta}");
+//Console.WriteLine($"Agência: {contaDoLeonardo.numeroAgencia}");
+//Console.WriteLine($"Saldo R$ {String.Format("{0:0.00}", contaDoLeonardo.saldo)}");
+
+//Console.ReadKey();
